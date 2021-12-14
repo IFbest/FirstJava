7b5a2b1219ab342646aea9194d2a2c541ffc6aba
YU <YU@220.74.102.130>
2021-12-14 19:14:30 +0900

    d

--- src/Test/Circle.java
+++ src/Test/Circle.java
@@ -3,17 +3,18 @@ package Test;
 public class Circle {
 	//PI라는 이름으로 3.14로 초기화된 정적 변수
 	public static final double PI =3.14;
-    public double radius;//radius를 무엇으로 변경? this가 private를 의미
+    private double radius;//radius를 무엇으로 변경? this가 private를 의미
     //원의 반지름을 매개 변수로 받는 생성자를 작성
     public Circle() {
-    	this.radius = radius;//this means private
+    //public Circle(double radius) {
+    	//this.radius = radius;//this means private
     }
     //접근자 메소드인 getRadius()를 작성
     public double getRadius() {
     	return radius;
     }
     //설정자 메소드인 setRadius()를 작성
-    public void setRadius(double radius,int i) {//before) double
+    public void setRadius(double radius, double b) {//before) double
     	this.radius = radius;
     }
     //PI값을 반환하는 정적 메소드 getPI를 작성
